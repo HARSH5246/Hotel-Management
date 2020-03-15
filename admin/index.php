36e8edc089144172e097c1ea600dfa3a5aaaca1f
Harsh Jain <46573414+HARSH5246@users.noreply.github.com>
2020-03-15 23:22:37 +0530

    Update Login Page

--- admin/index.php
+++ admin/index.php
@@ -1,48 +1,51 @@
 <?php  
-   session_start();  
-   if(isset($_SESSION["user"]))  
-   {  
-        header("location:home.php");  
-   }  
-?>
-
+    session_start();  
+    if(isset($_SESSION["user"]))  
+    {  
+      header("location:home.php");  
+    }  
+ ?>
 <!DOCTYPE html>
-<html >
+<html>
+
 <head>
     <meta charset="UTF-8">
     <title>KNIGHTS INN ADMIN</title>
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <div id="clouds">
-      	<div class="cloud x1"></div>
-      	<div class="cloud x2"></div>
-      	<div class="cloud x3"></div>
-      	<div class="cloud x4"></div>
-      	<div class="cloud x5"></div>
+        <div class="cloud x1"></div>
+        <!-- for multiple clouds -->
+        <div class="cloud x2"></div>
+        <div class="cloud x3"></div>
+        <div class="cloud x4"></div>
+        <div class="cloud x5"></div>
     </div>
 
     <div class="container">
         <div id="login">
-          <form method="post">
-              <fieldset class="clearfix">
-                  <p><span class="fontawesome-user"></span><input type="text"  name="user" value="Username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
-                  <p><span class="fontawesome-lock"></span><input type="password" name="pass"  value="Password" onBlur="if(this.value == '') this.value = 'Password'" onFocus="if(this.value == 'Password') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
-                  <p><input type="submit" name="sub"  value="Login"></p>
-              </fieldset>
-          </form>
+            <form method="post">
+                <fieldset class="clearfix">
+                    <p><span class="fontawesome-user"></span><input type="text" name="user" value="Username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
+                    <p><span class="fontawesome-lock"></span><input type="password" name="pass" value="Password" onBlur="if(this.value == '') this.value = 'Password'" onFocus="if(this.value == 'Password') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
+                    <p><input type="submit" name="sub" value="Login"></p>
+                </fieldset>
+            </form>
         </div> <!-- end login -->
     </div>
-    <div class="bottom"><h3><a href="../index.php">KNIGHTS INN HOMEPAGE</a></h3></div>
-  
+    
+    <div class="bottom">
+        <h3><a href="../index.php">KNIGHTS INN HOMEPAGE</a></h3>
+    </div>
+
 </body>
 </html>
 
 <?php
    include('db.php');
   
-   
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
@@ -55,6 +58,8 @@
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
          
