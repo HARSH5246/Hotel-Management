484fcdf108eed4ed787eda688d99f0c6596ce390
Harsh Jain <46573414+HARSH5246@users.noreply.github.com>
2020-03-24 23:00:04 +0530

    Updating Some Content

--- admin/usersetting.php
+++ admin/usersetting.php
@@ -1,28 +1,27 @@
 <?php  
-session_start();  
-if(!isset($_SESSION["user"]))
-{
- header("location:index.php");
-}
+    session_start();  
+    if(!isset($_SESSION["user"]))
+    {
+     header("location:index.php");
+    }
 
-ob_start();
+    ob_start();
 ?> 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-      <meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SUNRISE HOTEL</title>
-	<!-- Bootstrap Styles-->
+    <title>KNIGHTS INN HOTEL</title>
+	
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-     <!-- FontAwesome Styles-->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-        <!-- Custom Styles-->
     <link href="assets/css/custom-styles.css" rel="stylesheet" />
-     <!-- Google Fonts-->
-   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
     <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+
 </head>
+
 <body>
     <div id="wrapper">
         <nav class="navbar navbar-default top-navbar" role="navigation">
@@ -280,13 +279,10 @@ ob_start();
         </div>
      <!-- /. WRAPPER  -->
     <!-- JS Scripts-->
-    <!-- jQuery Js -->
+    
     <script src="assets/js/jquery-1.10.2.js"></script>
-      <!-- Bootstrap Js -->
     <script src="assets/js/bootstrap.min.js"></script>
-    <!-- Metis Menu Js -->
     <script src="assets/js/jquery.metisMenu.js"></script>
-      <!-- Custom Js -->
     <script src="assets/js/custom-scripts.js"></script>
     
    
